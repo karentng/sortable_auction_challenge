ab0395e0bd15a8ee73d9df11a5f651d1d5025a57
Karent Narvaez <karent.narvaez@asurion.com>
2020-01-19 20:39:32 -0500

    Main file

---
+++ auction/main.py
@@ -0,0 +1,22 @@
+import json
+import logging
+import sys
+import os
+
+from auction.run_auctions import load_configs, process_auctions
+
+
+def main():
+    """
+    Main function to load configurations and process the input file
+    """
+    # uncomment the line below to see logs
+    # logging.basicConfig(level=logging.INFO)
+    sites, adjustments = load_configs()
+    auctions = json.load(sys.stdin)
+    winners_auctions = process_auctions(sites, adjustments, auctions)
+    print(json.dumps(winners_auctions, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
